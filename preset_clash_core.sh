e0939e28333eab4edb9f9f29524f3e6ece551548
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-25 05:01:22 +0800

    Update preset_clash_core.sh

--- preset_clash_core.sh
+++ preset_clash_core.sh
@@ -13,6 +13,7 @@ mkdir -p $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-app-openclash/root/etc/openc
 core_path="$GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-app-openclash/root/etc/openclash/core"
 goe_path="$GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-app-openclash/root/etc/openclash"
 
+gunzip $core_path/clash_meta.gz
 cp -f $core_path/clash_meta $core_path/clash_meta
 
 wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > $goe_path/GeoIP.dat
