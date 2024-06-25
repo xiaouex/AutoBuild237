4ff069c30b679b728a5ac10c97c9c11fa846225d
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-26 00:05:52 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,7 +1,7 @@
 ./scripts/feeds install -a && rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/kenzo/luci-theme-argon
 rm -rf feeds/kenzo/luci-theme-argon-config
-cp -r feed/luci/theme/luci-theme-argon feeds/kenzo/luci-theme-argon
+cp -r feed/luci/themes/luci-theme-argon feeds/kenzo/luci-theme-argon
 cp -r feed/luci/applications/luci-theme-argon-config feeds/kenzo/luci-theme-argon-config
 
 #sed -i 's/PACKAGE_luci-lib-ipkg/PACKAGE_luci-base/g' $GITHUB_WORKSPACE/openwrt/package/feeds/small/luci-app-ssr-plus/Makefile 
