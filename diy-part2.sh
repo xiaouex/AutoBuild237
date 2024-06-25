0fc3456504ea4a9232c3c54f36a847f8bdb07c26
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-26 01:47:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/MineRouter/g' package/base-files/files/bin/config_generate
